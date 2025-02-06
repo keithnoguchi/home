5d124d3095df0b77310317c38e1c0db2686be3fa
Keith Noguchi <keith@noguchi.us>
2025-02-06 10:05:59 -0800

    config/nvim/lspconfig: clangd based LSP
    It hogs CPU a lot when I go through the linux kernel
    source code.  I'll give ccls a shot and see how it
    behaves.

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,6 +1,19 @@
 -- https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#configuration
 local lsp = require('lspconfig')
 
+-- C/C++
+lsp.clangd.setup{
+	cmd = {
+		'clangd',
+		'--background-index',
+		'--clang-tidy',
+		'--completion-style=detailed',
+		'--header-insertion=never',
+	},
+	filetypes = { 'c', 'cpp' },
+	root_dir = lsp.util.root_pattern('compile_commands.json', 'compile_flags.txt', '.git'),
+}
+
 -- Rust
 lsp.rust_analyzer.setup{
 	settings = {
