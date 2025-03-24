5619b13c6dadfc889a1d41e673eb1fe3ae52de3b
Keith Noguchi <keith@noguchi.us>
2025-03-24 15:03:48 -0700

    config/nvim/lspconfig: Add JS/TS LSP server

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -53,3 +53,14 @@ lsp.gopls.setup{
 		},
 	},
 }
+
+-- JS/TS
+lsp.ts_ls.setup {
+	on_attach = function(client, bufnr)
+		local opts = { noremap=true, silent=true, buffer=bufnr }
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+	end,
+	capabilities = require('cmp_nvim_lsp').default_capabilities(),
+}
